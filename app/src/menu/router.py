d490a64348baba5feedacadc42942e44fe340a8f
Artem Sorokin <85310022+juman-j@users.noreply.github.com>
2023-06-14 16:37:48 +0200

    made allergens in user_preferences nullable

--- app/src/menu/router.py
+++ app/src/menu/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
-# from sqlalchemy import select, insert
+
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
-# from src.models.models import restaurant, menu
+from src.models.models import food, user_preferences
 # from src.menu.schemas import 
 
 
@@ -13,7 +14,24 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_menu(session: AsyncSession = Depends(get_async_session)):
-    pass
+# @router.get("/")
+# async def get_menu(user_id: int, 
+#                    session: AsyncSession = Depends(get_async_session)):
+#     pass
 
+
+@router.get('/')
+async def ml(user_id: int, 
+       restaurant_id: int,
+       session: AsyncSession = Depends(get_async_session)):
+    stmt = select(food).where(food.c.restaurant_id == restaurant_id)
+    full_menu = await session.execute(stmt)
+    print(full_menu.all())
+    
+    stmt = select(user_preferences.c.preferred_ingredients).where(user_preferences.c.id == user_id)
+    user_ingr = await session.execute(stmt)
+    print(user_ingr.all())
+    
+    return None
+    
+    
